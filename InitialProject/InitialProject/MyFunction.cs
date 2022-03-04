a7fbe6ec28f6006d66ae08f82d755226a6aab19d
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-04 23:48:08 +0300

    Calculate for dummies

---
+++ InitialProject/InitialProject/MyFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InitialProject
+{
+    class MyFunction
+    {
+        public double Add(double x, double y) 
+        {
+            return (x + y);
+        }
+        public double Multiplication(double x, double y)
+        {
+            return (x * y);
+        }
+        public double Devision(double x, double y)
+        {
+            return (x / y);
+        }
+        public double Difference(double x, double y)
+        {
+            return (x - y);
+        }
+
+        public static implicit operator MyFunction(double v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InitialProject/InitialProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace InitialProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            string textFirstNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter second number: ");
+            string textSecondNumber = Console.ReadLine();
+
+            Console.WriteLine("Select an action  * / - + ");
+            string textMathematicalAction = Console.ReadLine();
+
+            MyFunction n = new MyFunction();
+
+            try
+            {
+                char mathematicalAction = char.Parse(textMathematicalAction);
+                double firstNumber = double.Parse(textFirstNumber);
+                double secondNumber = double.Parse(textSecondNumber);
+                
+                switch (mathematicalAction)
+                {
+                    case '*':
+                        Console.WriteLine($"The result of the multiplication of the numbers= {n.Multiplication(firstNumber, secondNumber)}");
+                        break;
+                    case '/':
+                        Console.WriteLine($"The result of the devision of the numbers = {n.Devision(firstNumber, secondNumber)}");
+                        break;
+                    case '+':
+                        Console.WriteLine($"The result of the sum of the numbers = {n.Add(firstNumber, secondNumber)}");
+                        break;
+
+                    case '-':
+                        Console.WriteLine($"The result of the difference of the numbers = {n.Difference(firstNumber, secondNumber)}");
+                        break;
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a number and try again");
+            }            
+        }
+    }
+}
