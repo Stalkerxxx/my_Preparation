acfa8c8dd2ee28d3d7d5c27238e4468b5ca309ab
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-09 20:33:48 +0300

    Decision Task02

---
+++ Task02(Exeption)/Task 02(Exeption)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_02_Exeption_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Hello, please enter parameter");
+                    string strParam = Console.ReadLine();
+                    int numParam = Int32.Parse(strParam);
+                    Console.WriteLine($"Your parameter multipleyd by two: {numParam * 2}");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter number and try again");
+                }
+            }
+        }
+    }
+}
