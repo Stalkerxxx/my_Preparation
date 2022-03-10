e9379f9c02ce3c196f5ab7e1327ed10f7c423055
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-10 21:34:41 +0300

    Decision Task03(two ctor & Exception)

--- Task03(classCar)/Task03(classCar)/Car.cs
+++ Task03(classCar)/Task03(classCar)/Car.cs
@@ -9,17 +9,36 @@ namespace Task03_classCar_
         private double Weight { get; set; }
         private int Speed { get; set; }
        
-        public Car(string _model, string _color, double _weight, int _speed)
+        public Car(string _model, string _color, 
+            double _weight, int _speed)
         {
             Model = _model;
             Color = _color;
             Weight = _weight;
             Speed = _speed;
         }
-        
-        public void PrintToConsole()
+        public Car(double _weight, int _speed)
         {
-            Console.WriteLine($"Model: {Model}, color: {Color}, weight: {Weight}, speed: {Speed}");
+            Weight = _weight;
+            Speed = _speed;
+        }
+        
+        public void PrintToConsole1()
+        {
+            Console.WriteLine(
+                $"Car 1: \n" + 
+                $"Model: {Model} \n" +
+                $"Color: {Color} \n" +
+                $"Weight: {Weight} \n" +
+                $"Speed: {Speed}"
+                );
+        }
+        public void PrintToConsole2()
+        {
+            Console.WriteLine(
+                $"Weight: {Weight} \n" +
+                $"Speed: {Speed}"
+                );
         }
     }
 }
--- Task03(classCar)/Task03(classCar)/Program.cs
+++ Task03(classCar)/Task03(classCar)/Program.cs
@@ -1,11 +1,43 @@
-﻿namespace Task03_classCar_
+﻿using System;
+namespace Task03_classCar_
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
             Car car1 = new ("Lada", "Black", 4.5, 240);
-            car1.PrintToConsole();
+            car1.PrintToConsole1();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter the model and color of the car:");
+                string modelCar2 = Console.ReadLine();
+                string colorCar2 = Console.ReadLine();
+                try
+                {
+                    if (String.IsNullOrEmpty(modelCar2) && String.IsNullOrEmpty(colorCar2))
+                    {
+                        Console.WriteLine("You were writing empty lines");
+                        break;
+                    }
+
+                    if (modelCar2.Length == 0 && colorCar2.Length == 0)
+                    {
+                        break;
+                    }
+                    Car car2 = new(1.2, 220);
+                    Console.WriteLine($"Car 2: \n" +
+                        $"Model: {modelCar2} \n" +
+                        $"Color: {colorCar2}");
+                    car2.PrintToConsole2();
+                    
+                    break;
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Please enter the model and color of the car");
+                }
+            }
         }
     }
 }
