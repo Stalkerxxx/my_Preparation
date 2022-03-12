4cc1001310e6829d1dfd5b1ed3560dc873dd89ef
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-12 13:50:17 +0300

    Decision Task04(static method)

---
+++ Task04(class House)/Task04(class House)/House.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task04_class_House_
+{
+    class House
+    {
+        public int floors;
+        public int year;
+        public string name;
+
+        public void OutputToTheConsole()
+        {
+            Console.WriteLine($"Purpose house: {name}\n" +
+                $"Year of construction: {year}\n" +
+                $"Number of floors: {floors}");
+        }
+        public static int AgeOfTheHouse(int ageHouse) 
+        {
+            DateTime dt = DateTime.Now;
+            int yearNow = dt.Year;
+            return yearNow - ageHouse;
+        }
+        public House()
+        {
+            Console.WriteLine("Empty constructor");
+        }
+        public House(int year, string name)
+        {
+            this.year = year;
+            this.name = name;
+        }
+        public House(int year, string name, int floors)
+        {
+            this.year = year;
+            this.name = name;
+            this.floors = floors;
+        }
+    }
+}
---
+++ Task04(class House)/Task04(class House)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task04_class_House_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House residentialBuilding = new (1996, "residential building");
+            House houseBusiness = new (2005, "business center", 65);
+            residentialBuilding.OutputToTheConsole();
+            Console.WriteLine($"Age business center: {House.AgeOfTheHouse(residentialBuilding.year)}");
+            houseBusiness.OutputToTheConsole();
+            Console.WriteLine($"Age business center: {House.AgeOfTheHouse(houseBusiness.year)}"); 
+        }
+    }
+}
