36d836eafb132628856d9cb6e067f57bd715ca1d
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-14 19:59:43 +0300

    Decision Task05(not tested operability)

---
+++ Task05(Inheritance)/Task05(Inheritance)/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task05_Inheritance_
+{
+    class Car
+    {
+        internal int weight;
+        internal string model ;
+        internal char color ;
+        internal int speedMax;
+
+        public void OutputToTheConsole() 
+        {
+            Console.WriteLine($"Model car: {model} \n" +
+                $"Color car: {color} \n" +
+                $"Maximum car speed: {speedMax} \n" +
+                $"Weight car: {weight} kg.s");
+        }
+    }
+}
---
+++ Task05(Inheritance)/Task05(Inheritance)/Truck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task05_Inheritance_
+{
+    class Truck : Car
+    {
+        private float numberOfWheels;
+        private int loadCapacity;
+
+        public float NewWheels(float x)
+        {
+            return numberOfWheels = x;
+        }
+
+        public int NewLoadCapacity(int x)
+        {
+            return loadCapacity = x;
+        }
+
+        public Truck(int weight, string model, char color, int speedMax)
+        {
+            this.weight = weight;
+            this.model = model;
+            this.color = color;
+            this.speedMax = speedMax;
+        }
+
+        public void OutToTheConsoleTruck()
+        {
+            Console.WriteLine($"Model car: {model} \n" +
+                $"Color car: {color} \n" +
+                $"Maximum car speed: {speedMax} \n" +
+                $"Weight car: {weight} kg.s \n" +
+                $"Number of wheels: {numberOfWheels} \n" +
+                $"Load capacity: {loadCapacity}");
+        }
+    }
+}
