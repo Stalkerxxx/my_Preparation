784761347c01a5c679af914ea4dadae58a217ce0
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-15 19:20:26 +0300

    Decision Task05(additional functionality)

--- Task05(Inheritance)/Task05(Inheritance)/Program.cs
+++ Task05(Inheritance)/Task05(Inheritance)/Program.cs
@@ -13,6 +13,14 @@ namespace Task05_Inheritance_
             truck1.NewLoadCapacity(400);
             truck1.NewWheels(4);
             truck1.OutToTheConsoleTruck();
+
+            Truck truck2 = new(15000, 'G', 8, 3000);
+            Console.WriteLine("\n Second truck: ");
+            Console.WriteLine("Enter model second truck:");
+            truck2.model = Console.ReadLine();
+            Console.WriteLine("Enter maximum speed second truck:");
+            truck2.speedMax = int.Parse(Console.ReadLine());
+            truck2.OutToTheConsoleTruck();
         }
     }
 }
