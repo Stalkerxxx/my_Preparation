243e66da54a44aca815f2a5cfd17ff4d3e8c8c41
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-15 18:48:50 +0300

    Decision Task05(properly)

--- Task05(Inheritance)/Task05(Inheritance)/Program.cs
+++ Task05(Inheritance)/Task05(Inheritance)/Program.cs
@@ -6,7 +6,13 @@ namespace Task05_Inheritance_
     {
         static void Main(string[] args)
         {
-
+            Truck truck1 = new(12690, "MAN", 'G', 120, 12, 3500);
+            Console.WriteLine("\n First truck: ");
+            truck1.OutToTheConsoleTruck();
+            Console.WriteLine("\n First truck modified:");
+            truck1.NewLoadCapacity(400);
+            truck1.NewWheels(4);
+            truck1.OutToTheConsoleTruck();
         }
     }
 }
--- Task05(Inheritance)/Task05(Inheritance)/Truck.cs
+++ Task05(Inheritance)/Task05(Inheritance)/Truck.cs
@@ -9,28 +9,31 @@ namespace Task05_Inheritance_
 
         public float NewWheels(float x)
         {
-            return numberOfWheels = x;
+            return numberOfWheels += x;
         }
 
         public int NewLoadCapacity(int x)
         {
-            return loadCapacity = x;
+            return loadCapacity += x;
         }
 
-        public Truck(int weight, string model, char color, int speedMax)
+        public Truck(int weight, string model, char color, int speedMax,
+            float numberOfWheels, int loadCapacity)
         {
             this.weight = weight;
             this.model = model;
             this.color = color;
             this.speedMax = speedMax;
+            this.numberOfWheels = numberOfWheels;
+            this.loadCapacity = loadCapacity;
         }
 
         public void OutToTheConsoleTruck()
         {
             Console.WriteLine($"Model car: {model} \n" +
                 $"Color car: {color} \n" +
-                $"Maximum car speed: {speedMax} \n" +
-                $"Weight car: {weight} kg.s \n" +
+                $"Maximum car speed: {speedMax} km/h \n" +
+                $"Weight car: {weight} kg. \n" +
                 $"Number of wheels: {numberOfWheels} \n" +
                 $"Load capacity: {loadCapacity}");
         }
