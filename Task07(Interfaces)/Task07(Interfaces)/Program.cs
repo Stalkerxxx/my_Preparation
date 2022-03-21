ed79e5e040c17f8c50a55da23f5a93c468fdd458
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-21 18:35:14 +0300

    Decision Task07(solved & tested)

---
+++ Task07(Interfaces)/Task07(Interfaces)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task07_Interfaces_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    interface IWalck
+    {
+        void OutToConsole();
+    }
+    class Dog : IWalck
+    {
+        public void OutToConsole() => Console.WriteLine("Dog");
+    }
+
+    class Cat : IWalck
+    {
+        public void OutToConsole() => Console.WriteLine("Cat");
+    }
+}
