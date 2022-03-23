5ae5ef2272fc749161e7fb112c20a88688e22905
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-03-23 18:44:14 +0300

    Decision Task08(solved & tested)

---
+++ Task08(Structurs)/Task08(Structurs)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task08_Structurs_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer1 = new();
+            computer1.BatteryLife = 180;
+            computer1.InstalledOs = "Windows";
+        }
+    }
+
+    public struct Computer
+    {
+        public string Processor{ get; set; }
+        public int BatteryLife { get; set; }
+        public double Weigth { get; set; }
+        public byte NumberOfCores { get; set; }
+        public string InstalledOs { get; set; }
+    }
+
+}
