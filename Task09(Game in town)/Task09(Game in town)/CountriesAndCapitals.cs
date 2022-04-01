c241c3362939de91dac0deb1c0f8142fa57f40bd
Stalkerxxx <knyazev.nikolay@yandex.ru>
2022-04-01 20:46:17 +0300

    Decision Task09(solved & tested)

---
+++ Task09(Game in town)/Task09(Game in town)/CountriesAndCapitals.cs
@@ -0,0 +1,13 @@
+﻿namespace Task09_Game_in_town_
+{
+    class CountriesAndCapitals
+    {
+        public string Countries { get; set; }
+        public string Capitals { get; set; }
+        public CountriesAndCapitals(string capitals, string countries)
+        {
+            Capitals = capitals;
+            Countries = countries;
+        }
+    }
+}
---
+++ Task09(Game in town)/Task09(Game in town)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task09_Game_in_town_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countriesAndCapitals = new Dictionary<string, string>
+            {
+                {"Russia", "Moscow"},
+                {"Hungary", "Budapest"},
+                {"USA", "Washington"},
+                {"Great Britain", "London"}
+            };           
+            
+            while (true)
+            {
+                Random random = new();
+                int index = random.Next(4);
+
+                KeyValuePair<string, string> kvp = countriesAndCapitals.ElementAtOrDefault(index);
+                string countrie = kvp.Key;
+                string capital = kvp.Value;
+                Console.WriteLine($"Please see the capital of this country: {countrie}");
+                string capitalUser = Console.ReadLine();
+
+                if (capitalUser == capital)
+                {
+                    Console.WriteLine("Your doing great"); 
+                }
+                else
+                {
+                    Console.WriteLine("You made a mistake");
+                    break;
+                }
+            }            
+        }
+    }
+}
